2433462b8742473109cad8290174c64c64e5250d
RaffDevs <rveronezdeveloper@gmail.com>
2023-06-01 19:15:24 -0300

    First Commit

---
+++ app/src/main/java/com/example/textcaptor/app/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.textcaptor.app.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.textcaptor.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/textcaptor/infra/database/AppDatabase.kt
@@ -0,0 +1,4 @@
+package com.example.textcaptor.infra.database
+
+class AppDatabase {
+}
