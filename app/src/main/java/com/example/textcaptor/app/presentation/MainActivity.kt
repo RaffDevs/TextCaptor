370f36dfbf78bbd64b2d78a64b2085a11225024b
RaffDevs <rveronezdeveloper@gmail.com>
2023-06-14 19:50:21 -0300

    feat: implementação do reconhecimento de imagem para texto

--- app/src/main/java/com/example/textcaptor/app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/textcaptor/app/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.textcaptor.app.presentation
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
@@ -12,6 +14,8 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.example.textcaptor.app.presentation.viewmodel.MainViewModel
@@ -24,7 +28,11 @@ import com.theartofdev.edmodo.cropper.CropImageView
 import dagger.hilt.android.HiltAndroidApp
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 @HiltAndroidApp
@@ -33,6 +41,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var bitmap: Bitmap
     private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var currentPhotoPath: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,70 +54,68 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-
-
-    }
-
-    private val openCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-
-        Log.d("GOMERY", "${result.resultCode}")
-
-
-        if (result.resultCode == RESULT_OK) {
-
-            val imageBitmap = result?.data?.getParcelableExtra<Bitmap>("data")
-
-
-            // Iniciar a atividade de recorte de imagem
-            if (imageBitmap != null) {
-                val imageUri = saveImageToCache(imageBitmap)
-
-                startImageCropper(imageUri)
-            }
-
-        } else if (result.resultCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            // Obter o resultado do recorte de imagem
-
-            val imageResult = CropImage.getActivityResult(result.data)
-
-
+    private val openCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
-                val resultURI = imageResult.uri
+                startImageCropper(Uri.fromFile(File(currentPhotoPath)))
+            }
+        }
+
+    private val cropImage =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val imageResult = CropImage.getActivityResult(result.data)
+
+                val resultUri = imageResult.uri
+
                 try {
-                    val source = ImageDecoder.createSource(this.contentResolver, resultURI)
+                    val source = ImageDecoder.createSource(this.contentResolver, resultUri)
                     bitmap = ImageDecoder.decodeBitmap(source)
                     getTextFromImage(bitmap)
-
                 } catch (error: Error) {
                     Log.d("Error", "onActivityResult: $error")
                 }
             }
-            // ...
-        } else if (result.resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-            Log.d("GOMERY", "ERROR: ${result.resultCode}")
         }
-    }
 
-    private fun saveImageToCache(bitmap: Bitmap): Uri {
-        val cacheDir = applicationContext.cacheDir
-        val imageFile = File(cacheDir, "captured_image.jpg")
-        FileOutputStream(imageFile).use { fos ->
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-        }
-        return FileProvider.getUriForFile(this, "com.example.fileprovider", imageFile)
-    }
 
     private fun dispatchTakePictureIntent() {
-
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        Log.d("CLICADO", "dispatchTakePictureIntent: ${takePictureIntent.resolveActivity(packageManager)}")
-
         if (takePictureIntent.resolveActivity(packageManager) != null) {
-            openCamera.launch(takePictureIntent)
+            val photoFile: File? = try {
+                createImageFile()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+                ex.printStackTrace()
+                null
+            }
+            photoFile?.also {
+                val photoURI: Uri =
+                    FileProvider.getUriForFile(this, "com.example.textcaptor.fileprovider", it)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                openCamera.launch(takePictureIntent)
+            }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(null)
+        return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            dispatchTakePictureIntent()
         }
     }
 
@@ -128,20 +135,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startImageCropper(imageURI: Uri) {
-        CropImage.activity(imageURI)
+        val intent = CropImage.activity(imageURI)
             .setGuidelines(CropImageView.Guidelines.ON)
-            .start(this)// Defina a proporção desejada para o recorte
+            .getIntent(this)
+        cropImage.launch(intent)
 
     }
 
     private fun setupListeners() {
         binding.btnScan.setOnClickListener {
-            dispatchTakePictureIntent()
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                dispatchTakePictureIntent()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_IMAGE_CAPTURE
+                )
+            }
         }
     }
-
-
-
 
 
 }
